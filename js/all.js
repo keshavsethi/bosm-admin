71b9da0fc5fe9c1d7d2d3fc6166c7feccadc0f69
keshavsethi <keshav.sethi0004@gmail.com>
2018-09-14 02:37:55 +0530

    Revert "Merge branch 'master' of https://github.com/keshavsethi/bosm-admin"
    This reverts commit f7db328ba472bf26e7d32344fae5aec7411b827a, reversing
    changes made to fffaf3d8e21f4331c07047db038fbc53b8e385c8.

--- js/all.js
+++ js/all.js
@@ -2,9 +2,10 @@
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-   
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+     
